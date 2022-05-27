5eabb9e4fe24980d6b49cef309f2ccd0196b4582
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-05-27 13:09:20 +0530

    administrator sidebar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
+import Signup from "./component/Auth/SignUp/index";
+import Login from "./component/Auth/Login/index.jsx";
+import AdminPage from "./component/Admin/AdminPage";
 
 function App() {
-    return <h1>Hello world!</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" exact element={<Signup />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/adminPage" exact element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/component/Auth/SignUp/index.jsx
@@ -0,0 +1,127 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./styles.module.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+const Signup = () => {
+  //   const [age, setAge] = React.useState("");
+
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    email: "",
+    phone: "",
+    role: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "";
+      const { data: res } = await axios.post(url, data);
+      navigate("/login");
+      console.log(res.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <div className={styles.signup_container}>
+      <div className={styles.signup_form_container}>
+        <div className={styles.left}>
+          <h4>Already have an account ? </h4>
+          <hr />
+          <Link to="/login">
+            <button type="button" className={styles.white_btn}>
+              Sign In
+            </button>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <form className={styles.form_container} onSubmit={handleSubmit}>
+            <h1>REGISTER</h1>
+            <input
+              type="username"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+              value={data.username}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className={styles.input}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className={styles.input}
+            />
+            <input
+              type="phone"
+              placeholder="Phone"
+              name="phone"
+              onChange={handleChange}
+              value={data.phone}
+              required
+              className={styles.input}
+            />
+
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Role</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={data.role}
+                label="Role"
+                type="role"
+                name="role"
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Student</MenuItem>
+                <MenuItem value={20}>Staff</MenuItem>
+                <MenuItem value={30}>Admin</MenuItem>
+              </Select>
+            </FormControl>
+
+            {error && <div className={styles.error_msg}>{error}</div>}
+            <button type="submit" className={styles.green_btn}>
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
