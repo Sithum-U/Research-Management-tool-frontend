5dfec593100d07f1fcbc98a2f02be88d205fc525
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-05-29 13:12:23 +0530

    supervisor detail table craeted

--- src/component/Admin/pages/Home.js
+++ src/component/Admin/pages/Home.js
@@ -1,3 +1,39 @@
+// import React, { useEffect, useState } from "react";
+// import { Image } from "cloudinary-react";
+
+// export default function Home() {
+//   const [imageIds, setImageIds] = useState();
+//   const loadImages = async () => {
+//     try {
+//       const res = await fetch("http://localhost:8000/api/images");
+//       const data = await res.json();
+//       setImageIds(data);
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+//   useEffect(() => {
+//     loadImages();
+//   }, []);
+//   return (
+//     <div>
+//       <h1 className="title">Cloudinary Gallery</h1>
+//       <div className="gallery">
+//         {imageIds &&
+//           imageIds.map((imageId, index) => (
+//             <Image
+//               key={index}
+//               cloudName="navindu"
+//               publicId={imageId}
+//               width="300"
+//               crop="scale"
+//             />
+//           ))}
+//       </div>
+//     </div>
+//   );
+// }
+
 import React, { useEffect, useState } from "react";
 import { Image } from "cloudinary-react";
 
@@ -5,8 +41,9 @@ export default function Home() {
   const [imageIds, setImageIds] = useState();
   const loadImages = async () => {
     try {
-      const res = await fetch("http://localhost:8000/api/images");
+      const res = await fetch("/api/images");
       const data = await res.json();
+      console.log(data);
       setImageIds(data);
     } catch (err) {
       console.error(err);
--- src/component/Admin/pages/Upload.js
+++ src/component/Admin/pages/Upload.js
@@ -1,13 +1,111 @@
+// import React, { useState } from "react";
+// import Alert from "./Alert";
+// import AdminPage from "../AdminPage/AdminPage";
+// import styles from "./styles.module.css";
+// import Button from "@mui/material/Button";
+// import Stack from "@mui/material/Stack";
+// import BackupIcon from "@mui/icons-material/Backup";
+// import Backup from "@mui/icons-material/Backup";
+// import { Grid } from "@mui/material";
+// import TextField from "@mui/material/TextField";
+
+// export default function Upload() {
+//   const [fileInputState, setFileInputState] = useState("");
+//   const [previewSource, setPreviewSource] = useState("");
+//   const [selectedFile, setSelectedFile] = useState();
+//   const [successMsg, setSuccessMsg] = useState("");
+//   const [errMsg, setErrMsg] = useState("");
+//   const handleFileInputChange = (e) => {
+//     const file = e.target.files[0];
+//     previewFile(file);
+//     setSelectedFile(file);
+//     setFileInputState(e.target.value);
+//   };
+
+//   const previewFile = (file) => {
+//     const reader = new FileReader();
+//     reader.readAsDataURL(file);
+//     reader.onloadend = () => {
+//       setPreviewSource(reader.result);
+//     };
+//   };
+
+//   const handleSubmitFile = (e) => {
+//     e.preventDefault();
+//     if (!selectedFile) return;
+//     const reader = new FileReader();
+//     reader.readAsDataURL(selectedFile);
+//     reader.onloadend = () => {
+//       uploadImage(reader.result);
+//     };
+//     reader.onerror = () => {
+//       console.error("AHHHHHHHH!!");
+//       setErrMsg("something went wrong!");
+//     };
+//   };
+
+//   const uploadImage = async (base64EncodedImage) => {
+//     try {
+//       await fetch("http://localhost:8000/api/upload", {
+//         method: "POST",
+//         body: JSON.stringify({ data: base64EncodedImage }),
+//         headers: { "Content-Type": "application/json" },
+//       });
+//       setFileInputState("");
+//       setPreviewSource("");
+//       setSuccessMsg("Image uploaded successfully");
+//     } catch (err) {
+//       console.error(err);
+//       setErrMsg("Something went wrong!");
+//     }
+//   };
+//   return (
+//     <div>
+//       <AdminPage />
+//       <div className={styles.signup_container}>
+//         <div className={styles.signup_form_container}>
+//           <div className={styles.right}>
+//             <h1 className="title">Upload Presentation / Document Templates</h1>
+//             <br />
+//             <Alert msg={errMsg} type="danger" />
+//             <Alert msg={successMsg} type="success" />
+//             <form onSubmit={handleSubmitFile} className="form">
+//               <Grid container spacing={2}>
+//                 <TextField
+//                   id="fileInput"
+//                   type="file"
+//                   name="image"
+//                   onChange={handleFileInputChange}
+//                   value={fileInputState}
+//                   className="form-input"
+//                 />
+//                 <Grid item xs={8}>
+//                   <Button
+//                     variant="contained"
+//                     type="submit"
+//                     endIcon={<Backup />}
+//                   >
+//                     Submit
+//                   </Button>
+//                 </Grid>
+//               </Grid>
+//             </form>
+//             {previewSource && (
+//               <img
+//                 src={previewSource}
+//                 alt="chosen"
+//                 style={{ height: "300px" }}
+//               />
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 import React, { useState } from "react";
-import Alert from "./Alert";
-import AdminPage from "../AdminPage/AdminPage";
-import styles from "./styles.module.css";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import BackupIcon from "@mui/icons-material/Backup";
-import Backup from "@mui/icons-material/Backup";
-import { Grid } from "@mui/material";
-import TextField from "@mui/material/TextField";
+import Alert from "../../Alert";
 
 export default function Upload() {
   const [fileInputState, setFileInputState] = useState("");
@@ -23,6 +121,7 @@ export default function Upload() {
   };
 
   const previewFile = (file) => {
+    console.log("fille previewed");
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onloadend = () => {
@@ -31,6 +130,7 @@ export default function Upload() {
   };
 
   const handleSubmitFile = (e) => {
+    console.log("submitting");
     e.preventDefault();
     if (!selectedFile) return;
     const reader = new FileReader();
@@ -45,8 +145,9 @@ export default function Upload() {
   };
 
   const uploadImage = async (base64EncodedImage) => {
+    console.log(base64EncodedImage);
     try {
-      await fetch("http://localhost:8000/api/upload", {
+      await fetch("/api/upload", {
         method: "POST",
         body: JSON.stringify({ data: base64EncodedImage }),
         headers: { "Content-Type": "application/json" },
@@ -61,45 +162,25 @@ export default function Upload() {
   };
   return (
     <div>
-      <AdminPage />
-      <div className={styles.signup_container}>
-        <div className={styles.signup_form_container}>
-          <div className={styles.right}>
-            <h1 className="title">Upload Presentation / Document Templates</h1>
-            <br />
-            <Alert msg={errMsg} type="danger" />
-            <Alert msg={successMsg} type="success" />
-            <form onSubmit={handleSubmitFile} className="form">
-              <Grid container spacing={2}>
-                <TextField
-                  id="fileInput"
-                  type="file"
-                  name="image"
-                  onChange={handleFileInputChange}
-                  value={fileInputState}
-                  className="form-input"
-                />
-                <Grid item xs={8}>
-                  <Button
-                    variant="contained"
-                    type="submit"
-                    endIcon={<Backup />}
-                  >
-                    Submit
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
-            {previewSource && (
-              <img
-                src={previewSource}
-                alt="chosen"
-                style={{ height: "300px" }}
-              />
-            )}
-          </div>
-        </div>
-      </div>
+      <h1 className="title">Upload an Image</h1>
+      <Alert msg={errMsg} type="danger" />
+      <Alert msg={successMsg} type="success" />
+      <form onSubmit={handleSubmitFile} className="form">
+        <input
+          id="fileInput"
+          type="file"
+          name="image"
+          onChange={handleFileInputChange}
+          value={fileInputState}
+          className="form-input"
+        />
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+      {previewSource && (
+        <img src={previewSource} alt="chosen" style={{ height: "300px" }} />
+      )}
     </div>
   );
 }
