52d914176c551d7530aa54249e8fc0d9cbf7afd4
Buddhini-123 <buddhiniup123@gmail.com>
2022-05-29 13:25:53 +0530

    changes to the app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 import PanelMember from "./component/panel_member/feedback";
 
 function App() {
     return (
-        <PanelMember/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/panelFeedback" exact element={<PanelMember />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
 export default App;
+
