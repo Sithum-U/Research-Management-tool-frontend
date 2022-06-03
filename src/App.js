42cd1b1d47c17cbcd84eec08439b3f1d780f5179
Sithum Udara <86145615+Sithum-U@users.noreply.github.com>
2022-06-03 14:14:01 +0530

    Supervisors (#3)
    * Adding Evaluation UI created
    
    * Display document evaluation marks UI created
    
    * Update evaluation created
    
    * Changes to display Evaluation UI
    
    * Footer added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Presentation from "./component/panel_member/Presentation";
 import PanelDash from "./component/panel_member/PanelDash";
 import Mail from "./component/panel_member/Mail";
 import ViewPresentationMarks from "./component/panel_member/viewPresentationMarks";
+import AddDocumentMarks from "./component/supervisor/documentEvaluation";
+import DisplayDocumentMarks from "./component/supervisor/displayDocMraks";
+import DocEvaluationUpdate from "./component/supervisor/updateDocMarks"
 
 function App() {
     return (
@@ -15,10 +18,12 @@ function App() {
                 <Route path="/panelDash" exact element={<PanelDash />} />
                 <Route path="/mail" exact element={<Mail />} />
                 <Route path="/viewpresentationmarks" exact element={<ViewPresentationMarks />} />
+                <Route path="/add" exact element={<AddDocumentMarks />} />
+                <Route path="/displayDocMarks" exact element={<DisplayDocumentMarks />} />
+                <Route path="/updateDocMarks/:id" exact element={<DocEvaluationUpdate />} />
             </Routes>
         </BrowserRouter>
     )
 }
 
 export default App;
-
---
+++ src/component/supervisor/documentEvaluation.jsx
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./styles.module.css";
+import Footer from "../Layout/Footer";
+
+const AddDocumentMarks = () => {
+  const [data, setData] = useState({
+    studentGrp: "",
+    completness: "",
+    corectness: "",
+    plagiarism: "",
+    total: "",
+    comments: ""
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:8000/api/docEvaluation/add";
+      const { data: res } = await axios.post(url, data);
+      navigate("/displayDocMarks");
+      console.log(res.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <div>
+    <div className={styles.signup_container}>
+      <div className={styles.signup_form_container}>
+        <div className={styles.right}>
+          <form className={styles.form_container} onSubmit={handleSubmit}>
+            <h1>Document Evaluation</h1>
+
+            <input
+              type="text"
+              placeholder="Student Group"
+              name="studentGrp"
+              onChange={handleChange}
+              value={data.studentGrp}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Completeness"
+              name="completness"
+              onChange={handleChange}
+              value={data.completness}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Correctness"
+              name="corectness"
+              onChange={handleChange}
+              value={data.corectness}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Plagiarism"
+              name="plagiarism"
+              onChange={handleChange}
+              value={data.plagiarism}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Total Marks"
+              name="total"
+              onChange={handleChange}
+              value={data.total}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Comments (optional)"
+              name="comments"
+              onChange={handleChange}
+              value={data.comments}
+              required
+              className={styles.input}
+            />
+            {error && <div className={styles.error_msg}>{error}</div>}
+            <button type="submit" className={styles.green_btn}>
+              Add Evaluation
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <Footer />
+    </div>
+  );
+};
+
+export default AddDocumentMarks;
